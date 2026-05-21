21dcc486b156884d0b793d7bcf98cc7e12c30bbe
AimanRazaq <aimanrazaq10@gmail.com>
2026-05-21 14:44:24 +0000

    database

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-// connectDB();
+connectDB();
 
 const app = express();
 app.use(cors());
